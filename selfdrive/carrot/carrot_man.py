846d6f4c0fd1f9d0f9c8e7c11ef46307f84d085a
ajouatom <ajouatom@gmail.com>
2024-11-20 14:07:36 +0900

    fix...

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -1364,7 +1364,7 @@ class CarrotServ:
     if CS is not None:
       #if source != self.source_last:
       #  self.gas_override_speed = 0
-      if desired_speed > 150 or source in ["cam", "section"] or CS.brakePressed:
+      if CS.vEgo < 0.1 or desired_speed > 150 or source in ["cam", "section"] or CS.brakePressed:
         self.gas_override_speed = 0
       elif CS.gasPressed:
         self.gas_override_speed = max(v_ego_kph, self.gas_override_speed)
